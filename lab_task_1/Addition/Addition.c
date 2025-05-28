1282891694f592e19565c3332f00740db4687b54
Sahil-Ishmam <sahiluddinishmam@gmail.com>
2025-05-28 14:26:11 +0600

    All four Operation added successfullygit add .git add .

---
+++ lab_task_1/Addition/Addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    // Addtion
+    int userId = 222115071;
+    int additional_count = 1000;
+    int final_result = userId + additional_count;
+    printf("Final Result : %d", final_result);
+}
---
+++ lab_task_1/Multiplication/Multiplication.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // Multiplication
+    int userId = 222115071;
+    int additional_count = 1000;
+    int final_result = userId * additional_count;
+    printf("Final Result : %d", final_result);
+    
+}
